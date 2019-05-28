6451b5c23e2342456ad90e14354c0d4c77d9db21
Jeremy Figueiredo <figueiredojeremy@gmail.com>
2019-05-28 11:43:14 +0200

    resolve algo

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const selectWilder = () => null;
+const selectWilder = (wilders, idSelected) => {
+  for(let i=0 ; i < wilders.length ; i++ ){
+    if(wilders[i].id === idSelected){
+      wilders[i].selected = true
+    }
+  }
+  return wilders
+};
 
 const groupBySchoolWilders = () => null;
 
